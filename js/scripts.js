3e4599bc27f0ecc3643924fbd9e8b7fb3f759841
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-21 09:13:11 -0700

    create constructor class and prototypes for pizza and toppings

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Pizza(size) {
+  this.pizzaSize = size;
+  this.cheese = [];
+  this.veggies =[]
+  this.meats = [];
+}
+Pizza.prototype.vegOptions = function(veg) {
+  this.vegToppings.push(veggie);
+}
+
+Pizza.prototype.meatOptions = function(meat) {
+  this.meatToppings.push(meat)
+}
