731f1a41b7c6572d681e06a3fa98eef4154147c4
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-21 10:45:48 -0700

    add protoype to calculate pizza cost

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,32 @@ function Pizza(size, cheese, cheeseType) {
   this.veggies =[]
   this.meats = [];
 }
-Pizza.prototype.vegOptions = function(veg) {
+Pizza.prototype.vegOptions = function (veg) {
   this.vegToppings.push(veggie);
 }
-
-Pizza.prototype.meatOptions = function(meat) {
-  this.meatToppings.push(meat)
+Pizza.prototype.meatOptions = function (meat) {
+  this.meatToppings.push(meat);
 }
+Pizza.prototype.pizzaCost = function (){
+  var pieCost = 0;
+  if (this.pizzaSize === "small") {
+    pieCost = 8;
+  } else if (this.pizzaSize === "medium") {
+    pieCost = 10;
+  } else if (this.pizzaSize === "large") {
+    pieCost = 13;
+  } else if (this.pizzaSize === "Extra Large") {
+    pieCost = 15;
+  }
+  this.veggies.forEach(function () {
+    cost += 1;
+  });
+  this.meats.forEach(function () {
+    cost += 1;
+  });
+  return pieCost;
+};
+
 $(document).ready(function(){
   var inputCheese = $('input[name="cheese"]:checked').val();
   var inputPizzaSize = $('input[name="pizza-size"]:checked').val();
